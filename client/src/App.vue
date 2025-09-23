7068a8b1436a4e83e6253bab549877b7bc75953b
Steph Forcier <steph.r.forcier@gmail.com>
2025-09-22 19:12:13 -0700

    Added vue and hooked up homepage to a vue component

---
+++ client/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hello">
+    <h2>{{ title }}</h2>
+    <p>This is a Vue component!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    title: {
+      type: String,
+      default: 'Hello from Vue'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.hello {
+  font-family: Arial, sans-serif;
+  color: #333;
+}
+</style>
---
+++ client/vite.config.js
@@ -0,0 +1,30 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  build: {
+    outDir: path.resolve(__dirname, '../client/static/assets'),
+    emptyOutDir: true,
+    rollupOptions: {
+      input: path.resolve(__dirname, 'src/main.js'),
+      output: {
+        entryFileNames: `[name].js`,
+        chunkFileNames: `[name].js`,
+        assetFileNames: `[name].[ext]`
+      }
+    }
+  }
+})
